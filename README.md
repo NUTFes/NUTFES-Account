30f91d2e2f2bd7c79b97f3831a6232ae93cce0d4
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-11-27 16:53:33 +0900

    [add] create user data (#1)

--- README.md
+++ README.md
@@ -1,16 +1,41 @@
 # stickee_account
 
 ## 初期手順
+以下の手順で環境を構築してください．
+1. クローン
 ```
-# クローン
 $ git clone git@github.com:NUTFes/stickee_account.git
-
-# ビルドするスクリプトに実行権限を与える
+```
+2. ビルドするスクリプトに実行権限を付与
+```
 $ chmod +x scripts/build.sh
-
-# コンテナをビルドする
+```
+3. コンテナをビルド
+```
 $ ./scripts/build.sh
-
-# コンテナを起動する
+```
+4. コンテナを起動
+```
 $ docker-compose up -d
+```
+
+## マイグレーション
+DBのマイグレーションはAlembicによってバージョン管理されています．
+
+したがって，モデルを追加した際はAlembicのmigrationバージョンを追加・更新する必要があります．
+
+バージョン追加方法は以下の通りです．
+
+1. バージョン作成
+```
+$ docker-compose run --rm backend alembic revision --autogenerate -m "<add  example column>" 
+```
+2. バージョンファイルを編集
+
+`backend/app/alembic/versions/yyyymmdd-HHMM_add_example_column.py`
+のファイルを`backend/app/db/models.py`を確認しながら編集
+
+3. マイグレーション
+```
+$ docker-compose run --rm backend alembic upgrade head
 ```
--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from datetime import datetime
 
 from .session import Base
 
@@ -6,10 +7,27 @@ from .session import Base
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    first_name = Column(String)
-    last_name = Column(String)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
+    id = Column('id', Integer, primary_key=True, unique=True, index=True, autoincrement=True)
+    number = Column('number', Integer, unique=True, index=True, nullable=False)
+    email = Column('email', String, unique=True, index=True, nullable=False)
+    first_name = Column('first_name', String)
+    last_name = Column('last_name', String)
+    first_name_kana = Column('first_name_kana', String)
+    last_name_kana = Column('last_name_kana', String)
+    hashed_password = Column('hashed_password', String, nullable=False)
+    sex = Column('sex', String)                   # 性別
+    birth = Column('birth', String)               # 生年月日
+    tel = Column('tel', String)                   # 電話番号
+    grade = Column('grade', String)               # 学年
+    major = Column('major', String)               # 学科
+    bureau = Column('bureau', String)             # 局
+    department = Column('department', String)     # 部門
+    position = Column('position', String)         # 役職
+
+    permission=Column('permission', String)     # 権限
+
+    is_active = Column('is_active', Boolean, default=True)
+    is_superuser = Column('is_superuser', Boolean, default=False)
+
+    updated_at=Column('updated_at', DateTime, default=datetime.now(), nullable=False)
+    created_at=Column('created_at', DateTime, default=datetime.now(), nullable=False)
