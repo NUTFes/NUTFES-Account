8f19f4d5f6788187a1679fdf56b284e8e9cbfb3e
TomoeYasuda <tomoe.pierrot3@gmail.com>
2022-02-03 23:08:23 +0900

    front

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+// import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,5 +1,36 @@
-export default function login(){
-  return (
-    <h1>hi</h1>
-  )
+// export default function login(){
+//   return (
+//     <h1>hi</h1>
+//   )
+// }
+import { ChakraProvider, Button,Input,Center,Box,Text } from '@chakra-ui/react';
+import * as React from 'react';
+
+class PulldownButton extends React.Component {
+  render() {
+    return (
+      <ChakraProvider>
+
+        <Box bgColor="purple.100" m="1%" p="1%" w="80%" borderWidth="3px" borderRadius="xl">
+        <Center>
+        <Text m="10" fontSize={{ base: "24px", md: "40px", lg: "56px" }}>
+          Stickee account
+        </Text>
+        </Center>
+          <Center>
+          <Input w='500px' h='50px' m="10"></Input>
+            </Center>
+            <Center>
+            <Input w='500px' h='50px' m="10"></Input>
+          </Center>
+          <Center>
+            <Button background='transparent' w='100px' h='10px' p='5' minWidth='0'>Sign In</Button>
+            <Button background='transparent' w='100px' h='10px' p='5' minWidth='0'>Sgin Up</Button>
+          </Center>
+        </Box>
+      </ChakraProvider>
+    );
+  }
 }
+
+export default PulldownButton;
