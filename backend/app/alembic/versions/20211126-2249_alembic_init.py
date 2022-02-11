24053cbdea5f09870ac8a153a09dbdf386aeac6f
TsubasaOura <Tsubasa.Gacct@gmail.com>
2022-02-12 00:01:46 +0900

    [mod] postgresql -> mariadb (#18)

--- backend/app/alembic/versions/20211126-2249_alembic_init.py
+++ backend/app/alembic/versions/20211126-2249_alembic_init.py
@@ -20,22 +20,22 @@ def upgrade():
         'user',
         sa.Column('id', sa.Integer, primary_key=True, unique=True, index=True, autoincrement=True),
         sa.Column('number', sa.Integer, unique=True, index=True, nullable=False),
-        sa.Column('email', sa.String, unique=True, index=True, nullable=False),
-        sa.Column('first_name', sa.String),
-        sa.Column('last_name', sa.String),
-        sa.Column('first_name_kana', sa.String),
-        sa.Column('last_name_kana', sa.String),
-        sa.Column('hashed_password', sa.String, nullable=False),
-        sa.Column('sex', sa.String),                   # 性別
-        sa.Column('birth', sa.String),               # 生年月日
-        sa.Column('tel', sa.String),                   # 電話番号
-        sa.Column('grade', sa.String),               # 学年
-        sa.Column('major', sa.String),               # 学科
-        sa.Column('bureau', sa.String),             # 局
-        sa.Column('department', sa.String),     # 部門
-        sa.Column('position', sa.String),         # 役職
+        sa.Column('email', sa.String(255), unique=True, index=True, nullable=False),
+        sa.Column('first_name', sa.String(255)),
+        sa.Column('last_name', sa.String(255)),
+        sa.Column('first_name_kana', sa.String(255)),
+        sa.Column('last_name_kana', sa.String(255)),
+        sa.Column('hashed_password', sa.String(255), nullable=False),
+        sa.Column('sex', sa.String(255)),                   # 性別
+        sa.Column('birth', sa.String(255)),               # 生年月日
+        sa.Column('tel', sa.String(255)),                   # 電話番号
+        sa.Column('grade', sa.String(255)),               # 学年
+        sa.Column('major', sa.String(255)),               # 学科
+        sa.Column('bureau', sa.String(255)),             # 局
+        sa.Column('department', sa.String(255)),     # 部門
+        sa.Column('position', sa.String(255)),         # 役職
 
-        sa.Column('permission', sa.String),     # 権限
+        sa.Column('permission', sa.String(255)),     # 権限
 
         sa.Column('is_active', sa.Boolean, default=True),
         sa.Column('is_superuser', sa.Boolean, default=False),
