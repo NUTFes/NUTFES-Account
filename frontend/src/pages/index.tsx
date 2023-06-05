3bd3f1c55de7d6130433bb94d57f15c8e3fe6d42
imaimai17468 <tosiki_801_life@outlook.jp>
2023-06-06 02:46:13 +0900

    [fix] add tailwind prettier

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <div className="w-full h-screen flex items-center justify-center">
-        <div className="bg-red-100 w-1/5 h-48 flex items-center justify-center">
-          <p className="text-red-300 text-2xl font-bold">tailwind-test-text</p>
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="flex h-48 w-1/5 items-center justify-center bg-red-100">
+          <p className="text-2xl font-bold text-red-300">tailwind-test-text</p>
         </div>
       </div>
     </main>
