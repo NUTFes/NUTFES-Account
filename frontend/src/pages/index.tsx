c822d3ca6e0221e811817fffed49acc4691d34f5
imaimai17468 <tosiki_801_life@outlook.jp>
2023-06-06 02:57:53 +0900

    [feat] add eslint rules

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,11 @@
-export default function Home() {
-  return (
-    <main>
-      <div className="flex h-screen w-full items-center justify-center">
-        <div className="flex h-48 w-1/5 items-center justify-center bg-red-100">
-          <p className="text-2xl font-bold text-red-300">tailwind-test-text</p>
-        </div>
+import { NextPage } from "next";
+
+export const Home: NextPage = () => (
+  <main>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex h-48 w-1/5 items-center justify-center bg-red-100">
+        <p className="text-2xl font-bold text-red-300">tailwind-test-text</p>
       </div>
-    </main>
-  );
-}
+    </div>
+  </main>
+);
