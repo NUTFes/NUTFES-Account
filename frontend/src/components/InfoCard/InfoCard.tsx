5540e2488d0a20baea886ddfd8cd3e8ae5f430b8
ryuseifujisaki <fujiryu1122@gmail.com>
2023-06-17 00:47:17 +0900

    [fix] design&Lint fix (#61)

--- frontend/src/components/InfoCard/InfoCard.tsx
+++ frontend/src/components/InfoCard/InfoCard.tsx
@@ -1,13 +1,7 @@
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Link from "@mui/material/Link";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
-import React from "react";
+import { ChevronRight as ChevronRightIcon } from '@mui/icons-material';
+import { Box, Link, Paper, Table, TableBody, TableCell, TableRow, Typography } from '@mui/material';
+import Router from 'next/router';
+import React from 'react';
 
 interface Props {
   data: {
@@ -21,35 +15,46 @@ interface Props {
   }
 }
 
+const handleClick = (link: string) => {
+  try {
+    Router.push(link);
+  } catch(err){
+    console.error("Not Exist Page")
+  }
+}
+
 function InfoCard({ data }: Props) {
   return (
-    <Card sx={{minWidth:723, maxWidth: 1023}}>
-      <CardContent>
+    <Paper sx={{minWidth: 723, maxWidth: 1023, px:2}}>
+      <Box sx={{minWidth:680, pt: 3, px: 2, pb: 1}}>
         <Typography variant="h5">
           {data.title}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {data.description}
+        <Typography variant="body2" color="text.secondary" sx={{pt: 1}}>
+          <span>
+            {data.description.split('\n').map((t, index)=> (<span key={index}>{t}<br /></span>))}
+          </span>
         </Typography>
-        <Table sx={{minWidth: 680}}>
+      </Box>
+        <Table>
           <TableBody>
             {data.table.map((row) => (
               <TableRow
                 hover
+                sx={{'&:last-child td, &:last-child th': { border: 0 }}}
+                onClick={() => handleClick(row.link)}
                 key={row.item}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
-                <TableCell align="left" style={{ width: 260}}>{row.item}</TableCell>
-                <TableCell align="left">{row.value}</TableCell>
-                <TableCell align="right">
-                  <Link href={row.link} color="inherit" underline="none" ><KeyboardArrowRightIcon/></Link>
+                <TableCell align="left" style={{width: 260}} sx={{color: "text.secondary"}}>{row.item}</TableCell>
+                <TableCell align="left" sx={{color: "text.secondary"}} >{row.value}</TableCell>
+                <TableCell align="right" sx={{color: "text.secondary"}} >
+                  <Link href={row.link} color="inherit" underline="none" ><ChevronRightIcon/></Link>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
-      </CardContent>
-    </Card>
+    </Paper>
   );
 }
 
