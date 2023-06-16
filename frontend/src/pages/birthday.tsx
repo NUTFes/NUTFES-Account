bbd2a5450ee37ebe43c9f2990eac2f3555f5c7a6
TsubasaOura <Tsubasa.Gacct@gmail.com>
2023-06-17 07:01:34 +0900

    [add] comming soon page

---
+++ frontend/src/pages/birthday.tsx
@@ -0,0 +1,36 @@
+import { Card, Button, Typography } from "@mui/material";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+export const Birthday: NextPage = () => {
+  const router = useRouter();
+  const handleClick = (link: string) => {
+    router.push(link).catch((error) => {
+      console.error(error);
+    });
+  };
+  return (
+    <main>
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="w-1/3">
+          <Card
+            sx={{
+              width: "100%",
+              height: "10rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              gap: "1rem",
+            }}
+          >
+            <Typography variant="h5">Comming soon Birthday Page</Typography>
+            <Button onClick={() => handleClick("/")}>Return to Home</Button>
+          </Card>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Birthday;
---
+++ frontend/src/pages/qrcode.tsx
@@ -0,0 +1,36 @@
+import { Card, Button, Typography } from "@mui/material";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+export const QRCode: NextPage = () => {
+  const router = useRouter();
+  const handleClick = (link: string) => {
+    router.push(link).catch((error) => {
+      console.error(error);
+    });
+  };
+  return (
+    <main>
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="w-1/3">
+          <Card
+            sx={{
+              width: "100%",
+              height: "10rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              gap: "1rem",
+            }}
+          >
+            <Typography variant="h5">Comming soon QRCode Page</Typography>
+            <Button onClick={() => handleClick("/")}>Return to Home</Button>
+          </Card>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default QRCode;
