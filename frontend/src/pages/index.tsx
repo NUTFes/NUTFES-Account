a7f49b2e8d1dfa87a8cdaf2e93050d8db6ceb5ce
TsubasaOura <Tsubasa.Gacct@gmail.com>
2023-06-21 00:12:59 +0900

    [fix]

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Card, Button, Typography } from "@mui/material";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useSession, signIn } from "next-auth/react";
@@ -16,7 +17,28 @@ const Home: NextPage = () => {
       }
     })();
   }, [session, router]);
-  return <main />;
+  return (
+    <main>
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="w-1/3">
+          <Card
+            sx={{
+              width: "100%",
+              height: "10rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              gap: "1rem",
+            }}
+          >
+            <Typography variant="h5">Tailwind + MUI</Typography>
+            <Button>test</Button>
+          </Card>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
