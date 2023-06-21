2b7de1ce8477c2ace161ffb7994036968f6be504
ryuseifujisaki <fujiryu1122@gmail.com>
2023-06-21 11:38:19 +0900

    [add] birthdayPageの枠組み完成(#68)

--- frontend/src/pages/birthday.tsx
+++ frontend/src/pages/birthday.tsx
@@ -1,33 +1,32 @@
-import { Card, Button, Typography } from "@mui/material";
+import { Paper } from "@mui/material";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useState } from "react";
+
+import { DetailLayout } from "@/components/layout";
 
 export const Birthday: NextPage = () => {
+  const [date, setDate] = useState(new Date());
+
   const router = useRouter();
   const handleClick = (link: string) => {
     router.push(link).catch((error) => {
       console.error(error);
     });
   };
+
+  const details = {
+    title: "生年月日",
+    description:
+      "生年月日は、Google サービスでアカウントのセキュリティ保護とカスタマイズに使用される場合があります。 この Google アカウントを企業または組織で使用する場合は、アカウントの管理者の生年月日を指定してください。",
+  };
+
   return (
     <main>
-      <div className="flex h-screen w-full items-center justify-center">
-        <div className="w-1/3">
-          <Card
-            sx={{
-              width: "100%",
-              height: "10rem",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-              gap: "1rem",
-            }}
-          >
-            <Typography variant="h5">Comming soon Birthday Page</Typography>
-            <Button onClick={() => handleClick("/")}>Return to Home</Button>
-          </Card>
-        </div>
+      <div>
+        <DetailLayout data={details}>
+          <Paper variant="outlined" sx={{ width: 550, height: 600 }} />
+        </DetailLayout>
       </div>
     </main>
   );
