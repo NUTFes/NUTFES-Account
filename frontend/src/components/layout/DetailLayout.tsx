441d040867eee75192dd31848a128581f7c37f12
ryuseifujisaki <fujiryu1122@gmail.com>
2023-06-24 00:28:59 +0900

    DetailLayoutのデザイン修正

--- frontend/src/components/layout/DetailLayout.tsx
+++ frontend/src/components/layout/DetailLayout.tsx
@@ -1,17 +1,15 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Typography } from "@mui/material";
+import { Typography, IconButton } from "@mui/material";
 import { useRouter } from "next/router";
 import React from "react";
 
 type Props = {
-  data: {
-    title: string;
-    description: string;
-  };
+  title: string;
+  description: string;
   children?: React.ReactNode;
 };
 
-const DetailLayout = ({ data, children }: Props) => {
+const DetailLayout = ({ title, description, children }: Props) => {
   const router = useRouter();
   // TODO: 認証状態を確認し、未認証の場合はログイン画面にリダイレクト
   const isAuthenticated = true;
@@ -27,20 +25,22 @@ const DetailLayout = ({ data, children }: Props) => {
       {/* TODO: childrenを持たないHeaderを配置 */}
       {/* SideTabは配置しない */}
 
-      <div className="fixed z-10 w-full border-b border-slate-300 bg-white">
-        <div className="m-auto flex h-14 items-center justify-center">
-          <button type="button" className="mr-1 h-12 w-12" onClick={backPage}>
+      <div className="sticky z-10 w-full border-b border-slate-300 bg-white">
+        <div className="flex h-14 items-center justify-center">
+          <IconButton aria-label="delete" color="primary" onClick={backPage}>
             <ArrowBackIcon color="action" />
-          </button>
-          <div className="w-3/5 w-[576px] text-3xl">{data.title}</div>
+          </IconButton>
+          <Typography sx={{ width: 550 }} style={{ fontSize: "28px" }}>
+            {title}
+          </Typography>
         </div>
       </div>
 
-      <div className="flex snap-y flex-col items-center justify-center pt-20">
+      <div className="flex flex-col items-center justify-center overflow-y-scroll px-6 pt-6">
         <div className="mb-8 max-w-xl">
-          <Typography color="text.secondary">{data.description}</Typography>
+          <Typography color="text.secondary">{description}</Typography>
         </div>
-        <div>{children}</div>
+        <div className="w-full max-w-xl">{children}</div>
       </div>
       {/* TODO: Footerを配置 */}
     </>
