95a2f7da2048317ca07e3e12e7a13470b23de057
TsubasaOura <Tsubasa.Gacct@gmail.com>
2023-06-25 19:24:48 +0900

    [fix]

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,44 +1,27 @@
 import { Card, Button, Typography } from "@mui/material";
 import { NextPage } from "next";
-import { useRouter } from "next/router";
-import { useSession, signIn } from "next-auth/react";
-import { useEffect } from "react";
 
-const Home: NextPage = () => {
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    (async () => {
-      if (session) {
-        await router.push("/");
-      } else {
-        await signIn("keycloak", { callbackUrl: `/` });
-      }
-    })();
-  }, [session, router]);
-  return (
-    <main>
-      <div className="flex h-screen w-full items-center justify-center">
-        <div className="w-1/3">
-          <Card
-            sx={{
-              width: "100%",
-              height: "10rem",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              flexDirection: "column",
-              gap: "1rem",
-            }}
-          >
-            <Typography variant="h5">Tailwind + MUI</Typography>
-            <Button>test</Button>
-          </Card>
-        </div>
+const Home: NextPage = () => (
+  <main>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="w-1/3">
+        <Card
+          sx={{
+            width: "100%",
+            height: "10rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
+          <Typography variant="h5">Tailwind + MUI</Typography>
+          <Button>test</Button>
+        </Card>
       </div>
-    </main>
-  );
-};
+    </div>
+  </main>
+);
 
 export default Home;
