c92f7e338139dacc527f720a4038edcca69f9319
TsubasaOura <Tsubasa.Gacct@gmail.com>
2023-07-02 22:46:31 +0900

    DEMOデータ追加

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -4,9 +4,10 @@ import KeycloakProvider from "next-auth/providers/keycloak";
 export default NextAuth({
   providers: [
     KeycloakProvider({
-        clientId: process.env.KEYCLOAK_ID!,
-        clientSecret: process.env.KEYCLOAK_SECRET!,
-        issuer: process.env.KEYCLOAK_ISSUER!,
+      clientId: process.env.KEYCLOAK_ID!,
+      clientSecret: process.env.KEYCLOAK_SECRET!,
+      issuer: process.env.KEYCLOAK_ISSUER!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET!,
 });
