b5dc751e09ef1e472895a1af938d18d882328c81
TsubasaOura <Tsubasa.Gacct@gmail.com>
2023-07-11 00:06:55 +0900

    next auth url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
