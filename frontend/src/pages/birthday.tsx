eca76befc416d831ad1bd4f9432f656da9ef952a
ryuseifujisaki <fujiryu1122@gmail.com>
2023-07-12 12:24:34 +0900

    [fix] デザインの修正(#87)

--- frontend/src/pages/birthday.tsx
+++ frontend/src/pages/birthday.tsx
@@ -2,22 +2,18 @@ import { Button, Box, Stack } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
-import ja from "date-fns/locale/ja";
+import { ja } from "date-fns/locale";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 import { DetailLayout } from "@/components/layout";
+import { DETAIL_PAGE_CONSTANTS } from "@/constants/detailPage";
 
 export const Birthday: NextPage = () => {
   const router = useRouter();
 
-  const details = {
-    // TODO: 今後constansに移行する
-    title: "生年月日",
-    description:
-      "生年月日は、Google サービスでアカウントのセキュリティ保護とカスタマイズに使用される場合があります。 この Google アカウントを企業または組織で使用する場合は、アカウントの管理者の生年月日を指定してください。",
-  };
+  const details = DETAIL_PAGE_CONSTANTS.birthday;
 
   // TODO: APIから取得してくる機能の実装
   const getBirthday = (): Date => new Date(2020, 8, 21, 21, 10, 5);
@@ -40,7 +36,7 @@ export const Birthday: NextPage = () => {
   return (
     <DetailLayout title={details.title} description={details.description}>
       <div className="pt-4">
-        <div className="h-screen w-full items-center justify-center">
+        <div className="h-auto w-full items-center justify-center">
           <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}>
             <Box sx={{ display: "flex", justifyContent: "center" }}>
               <DateCalendar value={birthday} onChange={handleBirthdayChange} />
@@ -53,8 +49,8 @@ export const Birthday: NextPage = () => {
             sx={{
               width: 1,
               maxWidth: 589,
-              position: "absolute",
-              bottom: "-200px",
+              position: "relative",
+              bottom: "40px",
             }}
           >
             <Button onClick={cancell}>キャンセル</Button>
